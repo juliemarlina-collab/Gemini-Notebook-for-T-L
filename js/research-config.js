5c5a806330732d8b3e3007dd4700614bdfb5aac0
Jules Hasan <juliemarlina@polipd.edu.my>
2026-08-26 07:50:18 +0800

    Add files via upload

---
+++ js/research-config.js
@@ -0,0 +1,11 @@
+/*
+ * Paste the deployed Google Apps Script URL ending in /exec below.
+ * Keep this as a separate file so the research connection can be updated
+ * without editing the main webapp.
+ */
+window.RESEARCH_CONFIG = {
+  endpoint: '',
+  sessionCode: 'GNB-WEBINAR-2026',
+  webappVersion: '1.1'
+};
+
